efd25d503de1e1775cf5c4be1a730957cb4c1522
Farouq Umar <ufakz99@gmail.com>
2021-08-31 21:16:44 +0100

    feat: created http client struct

--- client/http.go
+++ client/http.go
@@ -1,1 +1,15 @@
 package client
+
+import "net/http"
+
+type HTTPClient struct {
+	client     *http.Client
+	BackendURI string
+}
+
+func NewHTTPClient(uri string) HTTPClient {
+	return HTTPClient{
+		BackendURI: uri,
+		client:     &http.Client{},
+	}
+}
