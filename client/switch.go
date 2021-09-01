ce4431bc77e622fc61f2cbe46b4499b3a6db307e
Farouq Umar <ufakz99@gmail.com>
2021-09-01 14:13:53 +0100

    feat: added main file to for client functions

--- client/switch.go
+++ client/switch.go
@@ -1,1 +1,83 @@
 package client
+
+import (
+	"fmt"
+	"os"
+)
+
+type BackendHTTPClient interface {
+}
+
+type Switch struct {
+	client        BackendHTTPClient
+	backendAPIURL string
+	commands      map[string]func() func(string) error
+}
+
+func NewSwitch(uri string) Switch {
+	httpClient := NewHTTPClient(uri)
+	s := Switch{
+		client:        httpClient,
+		backendAPIURL: uri,
+	}
+	s.commands = map[string]func() func(string) error{
+		"create": s.create,
+		"edit":   s.edit,
+		"fetch":  s.fetch,
+		"delete": s.delete,
+		"health": s.health,
+	}
+	return s
+}
+
+func (s Switch) Switch() error {
+	cmdName := os.Args[1]
+	cmd, ok := s.commands[cmdName]
+	if !ok {
+		return fmt.Errorf("invalid command: '%s'", cmdName)
+	}
+	return cmd()(cmdName)
+}
+
+func (s Switch) Help() {
+	var help string
+	for name := range s.commands {
+		help += name + "\t --help\n"
+	}
+	fmt.Printf("Usage of: %s:\n <command> [<args>]\n%s", os.Args[0], help)
+}
+
+func (s Switch) create() func(string) error {
+	return func(cmd string) error {
+		fmt.Println("create reminder")
+		return nil
+	}
+}
+
+func (s Switch) edit() func(string) error {
+	return func(cmd string) error {
+		fmt.Println("edit reminder")
+		return nil
+	}
+}
+
+func (s Switch) fetch() func(string) error {
+	return func(cmd string) error {
+		fmt.Println("fetch reminder")
+		return nil
+	}
+}
+
+func (s Switch) delete() func(string) error {
+	return func(cmd string) error {
+		fmt.Println("delete reminder")
+		return nil
+	}
+}
+
+func (s Switch) health() func(string) error {
+	return func(cmd string) error {
+		fmt.Println("health reminder")
+		return nil
+	}
+}
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,29 @@
-package client
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"reminders/client"
+)
+
+var (
+	backendURIFlag = flag.String("backend", "http://localhost:8080", "Backend API URL")
+	helpFlag       = flag.Bool("help", false, "Display a helpful message")
+)
+
+func main() {
+	flag.Parse()
+	s := client.NewSwitch(*backendURIFlag)
+
+	if *helpFlag || len(os.Args) == 1 {
+		s.Help()
+		return
+	}
+
+	err := s.Switch()
+	if err != nil {
+		fmt.Printf("cmd switch error: %s", err)
+		os.Exit(2)
+	}
+}
